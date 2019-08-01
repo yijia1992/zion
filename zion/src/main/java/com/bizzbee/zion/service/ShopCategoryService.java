a49b71e75132baf76b5cd4e45424b5f2dd59bc5f
bizzbee <bizzbee@163.com>
2019-08-01 22:05:49 +0800

    delete and move

--- zion/src/main/java/com/bizzbee/zion/service/ShopCategoryService.java
+++
@@ -1,9 +0,0 @@
-package com.bizzbee.zion.service;
-
-import java.util.List;
-
-import com.bizzbee.zion.entity.ShopCategory;
-
-public interface ShopCategoryService {
-	List<ShopCategory> getShopCategoryList(ShopCategory shopCategory);
-}
